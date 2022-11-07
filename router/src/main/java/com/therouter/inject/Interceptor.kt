6dda264faee11f9356db045073e22b4a510dbfc3
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2022-11-07 14:11:33 +0800

    兼容apt生成kotlin代码

--- router/src/main/java/com/therouter/inject/Interceptor.kt
+++ router/src/main/java/com/therouter/inject/Interceptor.kt
@@ -4,5 +4,5 @@ package com.therouter.inject
  * Created by ZhangTao on 17/8/1.
  */
 interface Interceptor {
-    fun <T> interception(clazz: Class<T>?, vararg params: Any?): T
+    fun <T> interception(clazz: Class<T>?, vararg params: Any?): T?
 }
--- router/src/main/java/com/therouter/router/autowired/DefaultUrlParser.kt
+++ router/src/main/java/com/therouter/router/autowired/DefaultUrlParser.kt
@@ -30,11 +30,11 @@ class DefaultUrlParser : AutowiredParser {
             return null
         }
 
-        if (value.javaClass.name.equals(transformNumber(type), true)) {
+        if (javaTypeEqualsKotlinType(value.javaClass.name, type)) {
             return value as T?
         }
 
-        if (value.javaClass.name.equals("java.lang.String")) {
+        if (value.javaClass.name == "java.lang.String" || value.javaClass.name == "kotlin.String") {
             try {
                 return transform(type, value.toString()) as T?
             } catch (e: NumberFormatException) {
@@ -42,47 +42,74 @@ class DefaultUrlParser : AutowiredParser {
         }
         return null
     }
+}
 
-    private fun transformNumber(type: String): String {
-        return when (type) {
-            "byte" -> "java.lang.Byte"
-            "short" -> "java.lang.Short"
-            "int" -> "java.lang.Integer"
-            "long" -> "java.lang.Long"
-            "float" -> "java.lang.Float"
-            "double" -> "java.lang.Double"
-            "boolean" -> "java.lang.Boolean"
-            "char" -> "java.lang.Character"
-            else -> type
-        }
+private fun javaTypeEqualsKotlinType(type1: String, type2: String) = when (transformNumber(type1)) {
+    "java.lang.Short" -> type2 == "kotlin.Short"
+    "java.lang.Byte" -> type2 == "kotlin.Byte"
+    "java.lang.Integer" -> type2 == "kotlin.Int"
+    "java.lang.Long" -> type2 == "kotlin.Long"
+    "java.lang.Float" -> type2 == "kotlin.Float"
+    "java.lang.Double" -> type2 == "kotlin.Double"
+    "java.lang.Boolean" -> type2 == "kotlin.Boolean"
+    "java.lang.Character" -> type2 == "kotlin.Char"
+    "java.lang.String" -> type2 == "kotlin.String"
+    else -> type1 == type2
+}
+
+private fun transformNumber(type: String): String {
+    return when (type) {
+        "byte" -> "java.lang.Byte"
+        "short" -> "java.lang.Short"
+        "int" -> "java.lang.Integer"
+        "long" -> "java.lang.Long"
+        "float" -> "java.lang.Float"
+        "double" -> "java.lang.Double"
+        "boolean" -> "java.lang.Boolean"
+        "char" -> "java.lang.Character"
+        else -> type
+    }
+}
+
+private fun transform(type: String, value: String) = when (type) {
+    "byte" -> value.toByte()
+    "java.lang.Byte" -> value.toByte()
+    "kotlin.Byte" -> value.toByte()
+    "short" -> value.toShort()
+    "java.lang.Short" -> value.toShort()
+    "kotlin.Short" -> value.toShort()
+    "int" -> value.toInt()
+    "java.lang.Integer" -> value.toInt()
+    "kotlin.Int" -> value.toInt()
+    "long" -> value.toLong()
+    "java.lang.Long" -> value.toLong()
+    "kotlin.Long" -> value.toLong()
+    "float" -> value.toFloat()
+    "java.lang.Float" -> value.toFloat()
+    "kotlin.Float" -> value.toFloat()
+    "double" -> value.toDouble()
+    "java.lang.Double" -> value.toDouble()
+    "kotlin.Double" -> value.toDouble()
+    "boolean" -> value.toBoolean()
+    "java.lang.Boolean" -> value.toBoolean()
+    "kotlin.Boolean" -> value.toBoolean()
+    "char" -> if (value.isNotEmpty()) {
+        value[0]
+    } else {
+        null
     }
 
-    private fun transform(type: String, value: String) =
-        when (type) {
-            "byte" -> value.toByte()
-            "java.lang.Byte" -> value.toByte()
-            "short" -> value.toShort()
-            "java.lang.Short" -> value.toShort()
-            "int" -> value.toInt()
-            "java.lang.Integer" -> value.toInt()
-            "long" -> value.toLong()
-            "java.lang.Long" -> value.toLong()
-            "float" -> value.toFloat()
-            "java.lang.Float" -> value.toFloat()
-            "double" -> value.toDouble()
-            "java.lang.Double" -> value.toDouble()
-            "boolean" -> value.toBoolean()
-            "java.lang.Boolean" -> value.toBoolean()
-            "char" -> if (value.isNotEmpty()) {
-                value[0]
-            } else {
-                null
-            }
-            "java.lang.Character" -> if (value.isNotEmpty()) {
-                value[0]
-            } else {
-                null
-            }
-            else -> null
-        }
+    "java.lang.Character" -> if (value.isNotEmpty()) {
+        value[0]
+    } else {
+        null
+    }
+
+    "kotlin.Char" -> if (value.isNotEmpty()) {
+        value[0]
+    } else {
+        null
+    }
+
+    else -> null
 }
