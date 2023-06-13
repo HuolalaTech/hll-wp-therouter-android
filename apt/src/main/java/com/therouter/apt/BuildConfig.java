e34219af4a0b16e6fa07d5d80d2fb76680231179
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-06-13 14:53:52 +0800

    同步1.1.4-rc4内容

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.0-beta2";
+    public static final String VERSION = "1.2.0-beta3";
     public static final String NAME = "apt";
 
     private BuildConfig() {
--- plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.plugin;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.0-beta2";
+    public static final String VERSION = "1.2.0-beta3";
     public static final String NAME = "plugin-agp8";
 
     private BuildConfig() {
