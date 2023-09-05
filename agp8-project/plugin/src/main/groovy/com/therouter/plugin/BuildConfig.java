deb582b45754528622395be98a4b57a1275fdd58
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-09-05 15:21:20 +0800

    升级版本号

--- agp8-project/plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ agp8-project/plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.plugin;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.0-rc2";
+    public static final String VERSION = "1.2.0-rc3";
     public static final String NAME = "plugin-agp8";
 
     private BuildConfig() {
