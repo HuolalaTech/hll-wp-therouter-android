6976ceb60ce235cfb3cc0cd32e29403b75ac8bab
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-10-24 14:30:00 +0800

    更新版本号

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.0-alpha6";
+    public static final String VERSION = "1.2.0-rc7";
     public static final String NAME = "apt";
 
     public static final boolean DEBUG = false;
