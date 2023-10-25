79a856d9619a13fc7c2f114505741a89951dcf9b
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2023-10-25 10:20:39 +0800

    新增测试用例

--- app/src/main/java/com/therouter/app/flowtask/TestLateInit.kt
+++ app/src/main/java/com/therouter/app/flowtask/TestLateInit.kt
@@ -3,8 +3,14 @@ package com.therouter.app.flowtask
 import android.content.Context
 import android.util.Log
 import com.therouter.app.flowtask.lifecycle.FlowTask
+import com.therouter.flow.TheRouterFlowTask
 
 @FlowTask(taskName = AppFlowTask.TEST_LATE_1)
 fun init(ctx: Context) {
     Log.i("AppFlowTask.TEST_LATE_1", "packageName:${ctx.packageName},这是延迟初始化的代码1")
+}
+
+@FlowTask(taskName = AppFlowTask.TEST_LATE_2, dependsOn = TheRouterFlowTask.THEROUTER_INITIALIZATION)
+fun init2(ctx: Context) {
+    Log.i("AppFlowTask.TEST_LATE_2", "packageName:${ctx.packageName},这是延迟初始化的代码2")
 }
