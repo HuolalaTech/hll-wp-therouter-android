8eb867889fe85f47c312df2cdf3927ac5d3c3d48
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2024-01-05 17:33:23 +0800

    更新版本号1.2.2-rc4

--- agp8-project/plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ agp8-project/plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.plugin;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.2-rc3";
+    public static final String VERSION = "1.2.2-rc4";
     public static final String NAME = "plugin-agp8";
 
     private BuildConfig() {
