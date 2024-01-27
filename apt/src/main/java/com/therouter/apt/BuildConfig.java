1fec96e1c8983ab78be630e3bb58a6f1a8a0f0b8
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2024-01-27 16:20:01 +0800

    更新版本号1.2.2-rc6

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.2-rc5";
+    public static final String VERSION = "1.2.2-rc6";
     public static final String NAME = "apt";
 
     public static final boolean DEBUG = false;
