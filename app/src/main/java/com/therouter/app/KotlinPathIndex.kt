176e4c4e71b5b1553d85973af4c4816db89b874f
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2024-03-20 14:52:08 +0800

    修改路由path

--- app/src/main/java/com/therouter/app/KotlinPathIndex.kt
+++ app/src/main/java/com/therouter/app/KotlinPathIndex.kt
@@ -5,7 +5,7 @@ object KotlinPathIndex {
         const val HOME2 = "http://kymjs.com/therouter/home2"
         const val FRAGMENT_HOST = "http://kymjs.com/therouter/fragment_host_activity"
         const val FRAGMENT_TEST = "http://kymjs.com/therouter/test_fragment"
-        const val FRAGMENT_TEST2 = "http://kymjs.com/therouter/test_fragment2"
+        const val FRAGMENT_TEST2 = "/therouter/test_fragment2"
         const val VIEW_BINDING_FRAGMENT_TEST = "http://kymjs.com/therouter/viewbinding_fragment"
     }
 }
--- app/src/main/java/com/therouter/app/navigator/NavigatorFragment2.kt
+++ app/src/main/java/com/therouter/app/navigator/NavigatorFragment2.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.TextView
 import com.therouter.app.KotlinPathIndex.Test.FRAGMENT_TEST2
 import com.therouter.app.R
+import com.therouter.require
 import com.therouter.router.Autowired
 import com.therouter.router.Route
 
@@ -18,5 +19,10 @@ class NavigatorFragment2 : NavigatorFragment() {
         super.onViewCreated(view, savedInstanceState)
         val textView: TextView = view.findViewById<TextView>(R.id.textview12)
         textView.text = "子类 @Autowired 数据：$stringChildClassField"
+        require(
+            "数据在子类解析" == stringChildClassField,
+            "NavigatorFragment2",
+            "stringChildClassField数值不对"
+        )
     }
 }
