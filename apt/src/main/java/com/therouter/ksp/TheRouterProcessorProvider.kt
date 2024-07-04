4344d2ec859c9709ba3aae5b632887545373e4cc
tangwenjing <tangwenjing@wepie.com>
2024-07-04 17:41:08 +0800

    去除对auto-service的相关依赖

--- apt/src/main/java/com/therouter/ksp/TheRouterProcessorProvider.kt
+++ apt/src/main/java/com/therouter/ksp/TheRouterProcessorProvider.kt
@@ -1,11 +1,9 @@
 package com.therouter.ksp
 
-import com.google.auto.service.AutoService
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 
-@AutoService(SymbolProcessorProvider::class)
 class SimpleClassGeneratorProcessorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor =
         TheRouterSymbolProcessor(environment.codeGenerator, environment.logger)
