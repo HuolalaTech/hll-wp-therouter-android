7286160dd0e2865a08d8a6912d5a0bab7570bbf1
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2024-11-06 10:33:27 +0800

    降低gradle版本号到8.0.2

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.3-rc10";
+    public static final String VERSION = "1.2.3-rc11";
     public static final String NAME = "apt";
 
     public static final boolean DEBUG = false;
