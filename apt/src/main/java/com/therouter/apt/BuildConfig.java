a83499d8581640e580dd724cafd530a07568709d
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2024-11-14 17:02:44 +0800

    修改版本号1.2.3-rc17

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.3-rc16";
+    public static final String VERSION = "1.2.3-rc17";
     public static final String NAME = "apt";
 
     public static final boolean DEBUG = false;
--- plugin-agp8/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ plugin-agp8/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.plugin;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.3-rc16";
+    public static final String VERSION = "1.2.3-rc17";
     public static final String NAME = "plugin";
 
     private BuildConfig() {
