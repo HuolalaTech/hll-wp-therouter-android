7c0ab042a82b0d410f118c87665121810e7a43b9
zhangtao <kymjs@zhangtaodeMacBook-Pro-2.local>
2025-02-26 10:06:32 +0800

    升级版本号1.2.4

--- plugin-agp8/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ plugin-agp8/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.plugin;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.2.4-rc13";
+    public static final String VERSION = "1.2.4";
     public static final String NAME = "plugin";
 
     private BuildConfig() {
