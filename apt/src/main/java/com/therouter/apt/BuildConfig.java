fbbf32ab68678e5e900e7ac3b814265f0c284c7f
张涛 <kymjs123@gmail.com>
2025-04-30 11:17:46 +0800

    更新版本号1.3.0-rc3

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.3.0-rc2";
+    public static final String VERSION = "1.3.0-rc3";
     public static final String NAME = "apt";
 
     public static final boolean DEBUG = false;
