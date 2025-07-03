b7443ec62c4d5c185f7f8d8263132364b5d57fd1
张涛 <kymjs123@gmail.com>
2025-07-03 13:26:29 +0800

    更新rc5

--- apt/src/main/java/com/therouter/apt/BuildConfig.java
+++ apt/src/main/java/com/therouter/apt/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.apt;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.3.0-rc4";
+    public static final String VERSION = "1.3.0-rc5";
     public static final String NAME = "apt";
 
     public static final boolean DEBUG = false;
