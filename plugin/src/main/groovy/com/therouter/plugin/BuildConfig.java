69c7a9c49ca29abb91c001f32ec49367570c6d3a
张涛(tao281.zhang) <tao281.zhang@huolala.cn>
2025-07-11 11:03:23 +0800

    升级版本号1.3.0-rc6

--- plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
+++ plugin/src/main/groovy/com/therouter/plugin/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.therouter.plugin;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.3.0-rc5";
+    public static final String VERSION = "1.3.0-rc6";
     public static final String NAME = "plugin";
 
     private BuildConfig() {
