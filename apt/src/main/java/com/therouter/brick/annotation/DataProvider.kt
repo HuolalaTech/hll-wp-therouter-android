7f07f758561888baa5aee1c122ec8243c3e791d8
张涛 <kymjs123@gmail.com>
2026-01-13 13:20:08 +0800

    修改注解

--- apt/src/main/java/com/therouter/brick/annotation/DataProvider.kt
+++ apt/src/main/java/com/therouter/brick/annotation/DataProvider.kt
@@ -1,11 +1,14 @@
 package com.therouter.brick.annotation
 
+import java.lang.annotation.Repeatable
 import java.lang.annotation.Retention
 import java.lang.annotation.RetentionPolicy
 
 /**
  * Created by ZhangTao on 17/8/11.
  */
+@Repeatable(DataProviders::class)
+@kotlin.annotation.Repeatable
 @Retention(RetentionPolicy.SOURCE)
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.FUNCTION)
 annotation class DataProvider(val path: String, val fieldName: String = "", val priority: Int = 0)
